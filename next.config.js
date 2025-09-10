7558c19ec992f9b5b82c30a1e13d70f62b3da301
bibendi25 <edbirchmore@gmail.com>
2025-09-10 21:09:07 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true },
-  headers: async () => [
-    {
-      source: '/(.*)',
-      headers: [
-        { key: 'Content-Security-Policy', value: "frame-ancestors 'self' *.wixsite.com *.wix.com" }
-      ]
-    }
-  ]
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Content-Security-Policy', value: "frame-ancestors 'self' *.wixsite.com *.wix.com" }
+        ],
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
